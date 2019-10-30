ad902c55ee78d048aed9433365d862fb7a16183a
Tetram <tetram76@gmail.com>
2019-10-30 17:43:05 +0100

    launches subprocesses with the intended method

--- bdtheque/delphi/trunk/src/BD.Chromium/BD.Chromium.dpr
+++ bdtheque/delphi/trunk/src/BD.Chromium/BD.Chromium.dpr
@@ -1,12 +1,9 @@
 program BD.Chromium;
 
-{$APPTYPE CONSOLE}
-
 {$R *.res}
 
 uses
   WinApi.Windows,
-  uCEFApplication,
   BD.Utils.Chromium in '..\Commun\Utils\BD.Utils.Chromium.pas';
 
 {$SetPEFlags IMAGE_FILE_LARGE_ADDRESS_AWARE}
--- bdtheque/delphi/trunk/src/Commun/Utils/BD.Utils.Chromium.pas
+++ bdtheque/delphi/trunk/src/Commun/Utils/BD.Utils.Chromium.pas
@@ -13,24 +13,36 @@ uses
   uCEFApplication;
 
 procedure InitializeChromium;
+var
+  IsSubProcess: Boolean;
 begin
+  IsSubProcess := SameText(ExtractFileName(ParamStr(0)), 'BD.Chromium.exe');
+
   GlobalCEFApp := TCefApplication.Create;
 
   // obligatoire pour permettre à bdtheque d'être lancé sans donner le chemin de démarrage
   // (= les dll de Chromium se trouvent toujours avec l'appli, on n'autorise pas à contourner leur emplacement)
   GlobalCEFApp.SetCurrentDir := True;
 
-  GlobalCEFApp.SingleProcess := {$IFDEF DEBUG}True{$ELSE}False{$ENDIF};
+  GlobalCEFApp.SingleProcess := {$IFDEF DEBUG}not IsSubProcess{$ELSE}False{$ENDIF};
   // utiliser un autre exe comme subprocess permet de ne pas avoir à initialiser Chromium dans le dpr
-  GlobalCEFApp.BrowserSubprocessPath := 'Chromium\BD.Chromium.exe';
+  if not GlobalCEFApp.SingleProcess then
+    if IsSubProcess then
+      GlobalCEFApp.BrowserSubprocessPath := ParamStr(0)
+    else
+      GlobalCEFApp.BrowserSubprocessPath := 'Chromium\BD.Chromium.exe';
 
   GlobalCEFApp.FrameworkDirPath := 'Chromium';
   GlobalCEFApp.ResourcesDirPath := 'Chromium';
   GlobalCEFApp.LocalesDirPath := 'Chromium';
   GlobalCEFApp.LocalesRequired := 'fr';
 
-  // puisqu'on n'utilise pas le même exe pour les subprocess, pas besoin de réagir au résultat du retour de la fonction
-  GlobalCEFApp.StartMainProcess;
+  // on n'utilise pas Application.ExeName pour éviter les uses trop gourmand (Forms)
+  if IsSubProcess then
+    GlobalCEFApp.StartSubProcess
+  else
+    // puisqu'on n'utilise pas le même exe pour les subprocess, pas besoin de réagir au résultat du retour de la fonction
+    GlobalCEFApp.StartMainProcess;
 end;
 
 procedure FinalizeChromium;
